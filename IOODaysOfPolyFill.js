f89ff34b78206794b8c75ee1915150231f189584
MD-AZMAL <azmalmd12@gmail.com>
2021-05-16 20:30:04 +0530

    message

---
+++ IOODaysOfPolyFill.js
@@ -0,0 +1,6 @@
+const IOODaysofPolyFill = function(dayNumber) {
+    console.log('\n\n-------\\ 100 Days of Polyfill /-------');
+    console.log(`Day: ${dayNumber} \n\n`);
+}
+
+module.exports = IOODaysofPolyFill;
